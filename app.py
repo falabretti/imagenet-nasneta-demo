68672e1696fd3d46527eda55cc27ee960ad0011a
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-06-26 16:55:54 -0300

    Show prediction result with matplotlib

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import tensorflow as tf
 import tensorflow_hub as hub
-from PIL import Image
 import numpy as np
-
-print(tf.__version__)
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def load_model():
     # https://tfhub.dev/google/imagenet/nasnet_large/classification/5
     model = hub.load('imagenet_nasnet_large_classification_5/')
     return model
+
 
 def load_classes():
     # https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt
@@ -17,11 +16,13 @@ def load_classes():
         classes = [c.replace('\n', '') for c in classes]
         return classes
 
+
 def load_image(path):
     image = Image.open(path)
     image = image.resize((331, 331), Image.Resampling.LANCZOS)
     image = np.apply_along_axis(lambda x: x / 255., 2, image)
     return image
+
 
 def predict(model, classes, image):
     predictions = model([image])
@@ -29,9 +30,16 @@ def predict(model, classes, image):
     result = classes[class_idx]
     return result
 
+
+def show_result(image, prediction):
+    plt.imshow(image)
+    plt.title(prediction, fontdict={ 'size': 20 })
+    plt.show()
+
+
 model = load_model()
 classes = load_classes()
-image = load_image('images/barn.jpg')
+image = load_image('images/chair.jpg')
 
-result = predict(model, classes, image)
-print(result)
+prediction = predict(model, classes, image)
+show_result(image, prediction)
